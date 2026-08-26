ae774bc84d703eddd3b526318a6624d12e5cc63e
Debjit <debjit.chongdar@gmail.com>
2026-08-26 14:43:31 +0530

    Chat Application with Ollama module

---
+++ SingleModuleAIChat/src/main/java/in/ai/practice/controller/ChatController.java
@@ -0,0 +1,25 @@
+package in.ai.practice.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chatResponse(@RequestParam("message") String message){
+        return chatClient.prompt(message).call().content();
+    }
+}
